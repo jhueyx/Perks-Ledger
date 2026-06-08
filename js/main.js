c08c9a65fabe82076cedf2193a872e2620206a85
Jason Huey <jason.huey1@gmail.com>
2026-06-07 18:00:50 -0700

    chore: deploy timestamp 2026-06-07 18:00

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { CARDS, CARD_LABELS, PREMIUM_CARD_CATALOG, POINTS_MULTIPLIERS, TRANSFER_PARTNERS } from './cards.js';
-const DEPLOY_DATE='2026-06-06 21:15';
+const DEPLOY_DATE='2026-06-07 18:00';
 import { state, CY, CM, MONTHS, MONTHS_FULL, sb, freshDATA, STORAGE_KEY, escapeHtml, SUPABASE_URL, SUPABASE_KEY } from './state.js';
 import {
   toggle, scheduleSave, setSave, syncFromSupabase,
